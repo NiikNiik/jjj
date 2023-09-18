5b2eb5c638b87ee67501251dfe7a477181fa1d8c
Lamar1012 <126630656+Lamar1012@users.noreply.github.com>
2023-09-18 19:02:33 -0400

    commit url page

--- scores/urls.py
+++ scores/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('discover-vibes/', views.landing_page, name='landing_page'),
+    path('connect-vibes/', views.landing_page, name='landing_page'),
 ]
