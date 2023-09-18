dacf545f6ded17acc928e92c5afccf5c7825b7c0
Lamar1012 <126630656+Lamar1012@users.noreply.github.com>
2023-09-18 17:26:01 -0400

    put auth_token in bashrc

--- scores/views.py
+++ scores/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import SubscriberForm
+import os
 # from twilio.rest import Client
 from twilio.rest import Client
 from django.conf import settings
 
 # Create your views here.
+twilio_auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
 def landing_page(request):
     if request.method == 'POST':
@@ -12,9 +14,9 @@ def landing_page(request):
 
         if form.is_valid():
             form.save()
-            client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+            client = Client(settings.TWILIO_ACCOUNT_SID, twilio_auth_token)
             message = client.messages.create(
-                body = "Thank you for subscribing!",
+                body = "Thank you for subscribing! \U0001F680",
                 from_ = settings.TWILIO_PHONE_NUMBER,
                 to = form.cleaned_data['phone_number'] 
             )
